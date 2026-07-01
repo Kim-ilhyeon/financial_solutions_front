6413973db36a96e9947e74a384a9bf9a36f7d55d
Kim-ilhyeon <kih3915@naver.com>
2026-07-01 15:24:24 +0900

    third commit jQuery, Bootstrap-lecture code 26.07.01

--- JavaScript/todoList/script.js
+++ JavaScript/todoList/script.js
@@ -1,9 +1,27 @@
+// 전역 변수
+let todos = JSON.parse(localStorage.getItem('todos')) || [];
+let currentFilter = 'all';
+
+
+// 전역으로 사용할 요소 (반복적으로 사용이 필요한 요소)
+let todoInput;  // 입력 창
+let todoList;   // 할 일 목록
+let todoCount;  // 남은 할 일 갯수 (완료되지 않은 할 일)
+let filterBtns; // 필터 버튼들
+
+
 // DOM이 다 그려지면 실행.
 window.addEventListener("DOMContentLoaded", function() {
-    let todos = JSON.parse(localStorage.getItem('todos')) || [];
+    // dom요소 set
+    todoInput = document.getElementById('todo-input');
+    todoList = document.getElementById('todo-list');
+    todoCount = document.getElementById('todo-count');
+    filterBtns = document.querySelectorAll('.filter-buttons button');
 
-    let currentFilter = 'all';
-    const filterBtns = document.querySelectorAll('.filter-buttons button');
+    bindEvents();
+
+    renderTodos();
+
     filterBtns.forEach(function(btn) {
         btn.addEventListener('click', function() {
             // 클릭 시 모든 filter버튼의 active클래스 삭제
@@ -22,9 +40,6 @@ window.addEventListener("DOMContentLoaded", function() {
         });
     });
 
-    const addBtn = document.getElementById('add-btn');
-    const todoInput = document.getElementById('todo-input');
-    const todoList = document.getElementById('todo-list');
     
     // todo갯수 출력
     function countTodo() {
@@ -37,72 +52,101 @@ window.addEventListener("DOMContentLoaded", function() {
         localStorage.setItem('todos', JSON.stringify(todos));
     }
 
+    function getFilteredTodos() {
+        // filterBtn에 따른 todos 분기
+        switch (currentFilter) {
+            case 'all':
+                return todos;
+                break;
+            case 'progress':
+                return todos.filter(todo => !todo.isDone);
+                break;
+            case 'completed': 
+                return todos.filter(todo => todo.isDone);
+                break;
+        }
+    }
+
     function renderTodos() {
         todoList.textContent = '';
         
         let filteredTodos = [];
-        // filterBtn에 따른 todos 분기
-        switch (currentFilter) {
-            case 'all':
-                filteredTodos = todos;
-                break;
-            case 'progress':
-                filteredTodos = todos.filter(todo => !todo.isDone);
-                break;
-            case 'completed': 
-                filteredTodos = todos.filter(todo => todo.isDone);
-                break;
+        filteredTodos = getFilteredTodos();
+
+        if (filteredTodos.length === 0) {
+            emptyStateRender();
+        } else {
+            filteredTodos.forEach(function(todo) {
+                todoItemRender(todo);
+            });
         }
 
-
-        filteredTodos.forEach(function(todo) {
-            const newLi = document.createElement('li');
-            todoList.appendChild(newLi);
-            
-            const checkBox = document.createElement('div');
-            checkBox.classList.add('normal-checkBox');
-            newLi.appendChild(checkBox);
-            
-            const liContent = document.createElement('span');
-            liContent.textContent = todo.title;
-            newLi.appendChild(liContent);
-            
-            const delBtn = document.createElement('button');
-            delBtn.classList.add('btn-delete');
-            delBtn.textContent = '삭제';
-            newLi.appendChild(delBtn);
-            
-            // 완료(isDone)여부에 따른 클래스 추가
-            if (todo.isDone) {
-                newLi.classList.add('completed');
-                checkBox.classList.add('checked');
-            }
-
-            checkBox.addEventListener('click', function(e) {
-                e.stopPropagation();
-                console.log(todo.isDone);
-                todo.isDone = !todo.isDone;
-                checkBox.classList.toggle('checked');
-                newLi.classList.toggle('completed');
-                saveTodos();
-                renderTodos();
-                countTodo();
-            });
-
-
-            delBtn.addEventListener('click', function() {
-                todos = todos.filter((t) => todo.id !== t.id);
-                saveTodos();
-                renderTodos();
-                countTodo();
-            });
-        });
         countTodo();
     }
 
-    // 추가 클릭 시 할 일 추가
-    addBtn.addEventListener('click', function() {
-        const todo = todoInput.value;
+    // 랜더 시 할 일 목록이 없을 경우 보여줄 "할 일이 없습니다" 출력 함수
+    function emptyStateRender() {
+        const emptyEl = document.createElement('li');
+        emptyEl.classList.add('empty-state');
+        emptyEl.textContent = '할 일이 없습니다.';
+        todoList.appendChild(emptyEl);
+    }
+
+    // todoItem을 그리는 함수
+    function todoItemRender(todo) {
+        const newLi = document.createElement('li');
+        todoList.appendChild(newLi);
+        
+        const checkBox = document.createElement('div');
+        checkBox.classList.add('normal-checkBox');
+        newLi.appendChild(checkBox);
+        
+        const liContent = document.createElement('span');
+        liContent.textContent = todo.title;
+        newLi.appendChild(liContent);
+        
+        const delBtn = document.createElement('button');
+        delBtn.classList.add('btn-delete');
+        delBtn.textContent = '삭제';
+        newLi.appendChild(delBtn);
+        
+        // 완료(isDone)여부에 따른 클래스 추가
+        if (todo.isDone) {
+            newLi.classList.add('completed');
+            checkBox.classList.add('checked');
+        }
+
+        // 할 일 리스트의 체크 칸 클릭 시 이벤트 추가
+        checkBox.addEventListener('click', () => changeCompleted(todo.isDone, e));
+
+        // 삭제 버튼 클릭 시 이벤트 추가
+        delBtn.addEventListener('click', () => deleteTodo(todo.id));
+    }
+
+    // 삭제 클릭 시
+
+    // 할 일 목록 체크 시 완료여부(isDone) 변경하는 함수
+    function changeCompleted(isDone, e) {
+        e.stopPropagation();
+        isDone = !isDone;
+        checkBox.classList.toggle('checked');
+        newLi.classList.toggle('completed');
+        saveTodos();
+        renderTodos();
+    }
+
+    // 삭제 버튼 클릭 시 해당 할 일 삭제하는 함수
+    function deleteTodo(id) {
+        todos = todos.filter((t) => id !== t.id);
+        saveTodos();
+        renderTodos();
+    }
+
+    
+    
+    // 입력 창의 입력된 할 일을 가져와 todos에 추가하는 함수
+    function addTodo() {
+            const todo = todoInput.value;
         if (!todo) {
             alert('할 일을 입력하세요.!!!');
             return;
@@ -119,21 +163,27 @@ window.addEventListener("DOMContentLoaded", function() {
         todoInput.value = '';
         saveTodos();
         renderTodos();
-        countTodo();
-    });
-    
-    // 완료된 목록 삭제
-    const deleteAll = document.getElementById('delete-all');
-    deleteAll.addEventListener('click', function() {
+    }
+
+    // 완료된 목록 삭제하는 함수
+    function deleteCompletedAll() {
         const completedTodos = todos.filter(todo => !todo.isDone);
         localStorage.setItem('todos', JSON.stringify(completedTodos));
         todos = completedTodos;
         renderTodos();
         countTodo();
-    });
-    
-    
-    
+    }
+
+    // dom요소와 이벤트 핸들러를 연결해주는 함수
+    function bindEvents() {
+        const addBtn = document.getElementById('add-btn');
+        // 추가 클릭 시 할 일 추가
+        addBtn.addEventListener('click', addTodo);
+
+        // 완료된 목록 삭제
+        const deleteCompletedTodos = document.getElementById('delete-completed-btn');
+        deleteCompletedTodos.addEventListener('click', deleteCompletedAll);
+    }
     
     
     
